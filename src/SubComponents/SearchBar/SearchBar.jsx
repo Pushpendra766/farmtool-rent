195d7a7c37c6c0b2e1253c90184d30b04ca90681
Pushpendra Chandravanshi <pushpendrachandravanshi4@gmail.com>
2023-03-20 00:38:17 +0530

    translation

--- src/SubComponents/SearchBar/SearchBar.jsx
+++ src/SubComponents/SearchBar/SearchBar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BsSearch } from "react-icons/bs";
 import { MdMic } from "react-icons/md";
+import { useTranslation } from "react-i18next";
 
 const SearchBar = ({ additionalClass }) => {
+  const {t} = useTranslation();
   return (
     <div className={`w-full mx-auto mt-1 flex flex-row ${additionalClass}`}>
       <input
         className="rounded-md w-full px-2 py-1 text-[#000000] outline-0"
         type="text"
-        placeholder="Search for equipments or tools"
+        placeholder={t("search_placeholder")}
       />
       <span className="pt-2 cursor-pointer">
         <BsSearch className="relative right-6 text-primary" />
