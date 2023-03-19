ae77feb63577b08ea17a6bb87d2e097092eba91d
Anekant <Anekantjainsagar@gmail.com>
2023-03-19 23:55:49 +0530

    Added favicon

--- src/Screens/Category/CategoryScreen.jsx
+++ src/Screens/Category/CategoryScreen.jsx
@@ -10,7 +10,9 @@ const CategoryScreen = () => {
       <h1 className="text-center font-semibold text-dark-green md:pt-20 mobile:pt-28 md:text-2xl mobile:text-[26px]">
         Best of {params.name}
       </h1>
-      <p className="text-center md:text-lg mobile:text-[20px] mb-1 text-orange">103 Items</p>
+      <p className="text-center md:text-lg mobile:text-[20px] mb-1 text-orange">
+        103 Items
+      </p>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 w-10/12 md:w-11/12 sm:w-11/12 mx-auto">
         <Category />
         <Category />
