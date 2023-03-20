850bb5dccaa6b6ed686c441cbefcc62e2175b0a9
Shivam <shivam.yadav98939294@gmail.com>
2023-03-20 13:20:00 +0530

    pull req searchbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router";
 import { useTranslation } from "react-i18next";
 import {CgProfile} from "react-icons/cg"
 import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {BiLogIn} from 'react-icons/bi'
+
 const Navbar = ({logstatus,handleAuthentication}) => {
   const history = useNavigate();
   const { t } = useTranslation();
@@ -29,16 +31,17 @@ const Navbar = ({logstatus,handleAuthentication}) => {
         </div>
         <SearchBar additionalClass="hidden md:flex" />
         <Language />
-        < AiOutlineShoppingCart size={35} style={{transform:"translateY(3px)"}} onClick={() => history(logstatus?"/cart":"/login")}/>
+        < AiOutlineShoppingCart size={35} style={{transform:"translateY(3px) scale(1.3)"}} onClick={() => history(logstatus?"/cart":"/login")}/>
+         <BiLogIn   className="md:hidden" size={35} style={{transform:"translateY(3px) scale(1.3)"}} onClick={() => history(logstatus?"/cart":"/login")}/>
          {logstatus?(
                    <span className="flex flex-row gap-4 hidden md:flex">
                    <CgProfile size={30} style={{transform:"translateY(5px)"}} onClick={() => history("/profile")}/>
                    <button onClick={() => handleAuthentication(false)}>Logout</button>
                    </span>
          ):(
-        <span className="flex flex-row gap-4 hidden md:flex">
-          <button onClick={() => history("/login")}>{t("login")}</button>
-          <button onClick={() => history("/signup")}>{t("signup")}</button>
+        <span className="flex flex-row gap-4 hidden  md:flex">
+          <button className="w-[100px]"  onClick={() => history("/login")}>{t("login")}</button>
+          <button  className="w-[100px]" onClick={() => history("/signup")}>{t("signup")}</button>
         </span>)}
 
 
